3776015d04513e3b5b9c44932eca5fd7c6e0eb8f
joaquin.mesuro@gmail.com <joaquin.mesuro@gmail.com@6be89132-b271-449c-ffbe-d7097430caeb>
2013-04-26 16:02:13 +0000

    Temp, clase para la funcion que mapea enteros a string de expresiones

--- Fastest/src/compserver/tcasegen/strategies/SetLogGrammar/Test.java
+++ Fastest/src/compserver/tcasegen/strategies/SetLogGrammar/Test.java
@@ -1,32 +1,39 @@
 package compserver.tcasegen.strategies.SetLogGrammar;
 
 
+import java.util.HashMap;
+
 import javax.swing.tree.DefaultMutableTreeNode;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import compserver.tcasegen.strategies.SetLogGrammar.Temp;
 
 public class Test {
-
 	
+		
 	
 	public static void main(String[] args) {
 		
-		ANTLRInputStream input = new ANTLRInputStream("\\power (\\num \\cross (\\power A))");
+		ANTLRInputStream input = new ANTLRInputStream("\\power FT");
         TypeManagerLexer lexer = new TypeManagerLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         TypeManagerParser parser = new TypeManagerParser(tokens);
         parser.typeManage();
         DefaultMutableTreeNode root =  parser.getRoot();
-        
-        
-		String s = "[{X}]";
+//        
+//        
+//		String s = "[{X}]";
+//		
+//        ConstantCreator cc = new ConstantCreator(null, null, null, null);
+//		String salida = cc.getCte(s,root);
+//		
+//		System.out.println("ENTRADA\n" + s);
+//		System.out.println("SALIDA\n" + salida);
 		
-        ConstantCreator cc = new ConstantCreator(null, null, null, null);
-		String salida = cc.getCte(s,root);
-		
-		System.out.println("ENTRADA\n" + s);
-		System.out.println("SALIDA\n" + salida);
+		HashMap<String,String> tipos = new HashMap<String, String>();
+		tipos.put("FT", "EnumerationType:FT:{a,b,c}");
+		System.out.println("\naaaaaaaaaa " + Temp.f(root,1,tipos));
 		//System.out.println("\nasdssssssss\n" + getNotEqType("aaa,bbb,ccc,ddd","bbb,aaa,ddd"));
 	}
 
