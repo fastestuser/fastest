ba5138a40445c948db7d84e09e4edaa282c41325
joaquin.mesuro@gmail.com <joaquin.mesuro@gmail.com@6be89132-b271-449c-ffbe-d7097430caeb>
2013-07-19 13:03:15 +0000

    arreglado ExpressionDelimiter tomaba nomvar falsos

--- Fastest/src/compserver/axdef/ExpressionDelimiter.java
+++ Fastest/src/compserver/axdef/ExpressionDelimiter.java
@@ -2,13 +2,13 @@ package compserver.axdef;
 
 public class ExpressionDelimiter {
 
-
+	//dice si es el principio de un argumento
 	private static boolean finDeArg( int cantP, String pred, int i){
 		char c1 = pred.charAt(i); 
 		char c2 = i+1>=pred.length()?' ':pred.charAt(i+1);
 		return ( cantP == 0 && c1 != '~' && c1 != ' '  && (c2 == ' ' || c2 =='~') );
 	}
-	
+	//dice si es el final de un argumento
 	private static boolean prinDeArg(int cantP, String pred, int i){
 		char c1 = pred.charAt(i); 
 		char c2 = i+1>=pred.length()?' ':pred.charAt(i+1);
@@ -21,7 +21,10 @@ public class ExpressionDelimiter {
 		public String salida;
 	}
 	
-	
+	/*
+	* dada un string que comienza con nomvar, ej "fun sss (fun aa bb) dddd"
+	* devuelve "°fun ¬sss¬¬(°fun ¬aa¬¬bb¬°)¬° dddd"
+	*/
 	private static SPrima marcarPredPrima(String pred, String nomvar, int argc){
 		int argcint = argc; 
 		int length = pred.length();
@@ -30,10 +33,11 @@ public class ExpressionDelimiter {
 		String salida = "",prima = "";
 		int i,prinArg=0,iprimaf = 0;
 		SPrima sp = new SPrima();
-		char c ; 
+		char c ;
+		//boolean entrearg = true;
 		for ( i=0; i<length && argcint>0 ; i++){
 			c = pred.charAt(i);
-			if ( pred.substring(i).startsWith(nomvar) && prima.isEmpty() ){
+			if ( prinFun(pred,nomvar,i) && prima.isEmpty() ){
 				sp = marcarPredPrima(pred.substring(i),nomvar,argc);
 				prima =  pred.substring(prinArg,i) +  sp.salida ;
 				iprimaf = i + sp.i + nomvar.length()-1;
@@ -51,7 +55,8 @@ public class ExpressionDelimiter {
 			
 			if (prinDeArg(cantP,pred,i)){
 				salida += "¬"; 
-				prinArg = i+1;
+				prinArg = i;
+				//entrearg = false;
 			}
 			
 			if (finDeArg(cantP,pred,i) ){
@@ -62,6 +67,7 @@ public class ExpressionDelimiter {
 				else
 					salida += pred.substring(prinArg,i+1) + "¬" ;
 				argcint--;
+				//entrearg = true;
 			}
 		}
 		SPrima sps = new SPrima();
@@ -115,17 +121,23 @@ public class ExpressionDelimiter {
 //		
 //		return "°" + nomvar + salida + "°" + pred.substring(i,pred.length());
 //	}
-	
+	private static boolean prinFun(String pred, String nomvar, int i){
+		char csig = i+nomvar.length()<pred.length()?pred.charAt(i+nomvar.length()):' ';
+		return pred.substring(i).startsWith(nomvar) && (csig == ' ' || csig == '~');
+	}
 	public static String marcarPred(String pred, String nomvar, int argc){
-		int length = pred.length(),iprimaf=0;
+		int length = pred.length();
 		SPrima sp = new SPrima();
 		sp.salida = "";
 		String salida = "",pedazo="";
 		int c=0,f=0;
-		int i;
+		int i; 
+		// "sssssss f x (f x y) ddd f x y hhh"
+		// pedazo son los substring de pred que no son fun ej: sssssss,ddd,hhh
+		// c y f marcan el comienzo i final de cada pedazo.
 		for (i = 0;i<length;i++){
-		
-			if(pred.substring(i).startsWith(nomvar)){
+			
+			if(prinFun(pred,nomvar,i)){
 				f = i;
 				pedazo = pred.substring(c, f);
 				sp = marcarPredPrima(pred.substring(i),nomvar,argc);
@@ -136,10 +148,10 @@ public class ExpressionDelimiter {
 			
 		}
 		return salida + pred.substring(c,pred.length());
-	}
+	} 
 	
 	public static void main(String[] args) {
-		String s = marcarPred("f~x~(f g h) d f a b","f",2);
+		String s = marcarPred("f aa bb faild ~ (f aa dd) bb","f",2);
 		System.out.println(s);
 	}
 
