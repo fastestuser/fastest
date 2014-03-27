9dd10e7e6e77a40d4b02699746b4d7b7b70fbcd5
joaquin.mesuro@gmail.com <joaquin.mesuro@gmail.com@6be89132-b271-449c-ffbe-d7097430caeb>
2014-03-27 14:53:54 +0000

    agregado en ShowHelpCommand, y mejorado de la impresion de CTCPrinter

--- Fastest/src/client/blogic/testing/refinamiento/CTCPrinter.java
+++ Fastest/src/client/blogic/testing/refinamiento/CTCPrinter.java
@@ -14,29 +14,23 @@ public class CTCPrinter {
 	 */
 	public static String printCTC(String testName, ConcreteTCase ctc) {
 		String targetLanguaje = ctc.getLanguaje();
-//		if(targetLanguaje.equals("C"))
-//			return printCTCC(testName,ctc);
+		//		if(targetLanguaje.equals("C"))
+		//			return printCTCC(testName,ctc);
 		//else 
 		if(targetLanguaje.equals("Java")) // Ver consistencia may/min
 			return printCTCJava(testName, ctc);
 		return null;
 	}
-	
+
 	public static String printCTCJava(String testName, ConcreteTCase ctc) {
 		StringBuilder ctcString = new StringBuilder();		// EMPTY STRING
 
-		//--------------------------------------------------------------------------------
 		// CONCATENATES the TESTNAME
-		//--------------------------------------------------------------------------------
-		ctcString = ctcString.append("//--------------------------------------------------------------------------------" + "\n");
-		ctcString = ctcString.append(
-				"// " + testName + "\n");
-		ctcString = ctcString.append("//--------------------------------------------------------------------------------" + "\n");
-		ctcString = ctcString.append("\n");
-		//--------------------------------------------------------------------------------
+		ctcString.append("//--------------------------------------------------------------------------------" + "\n");
+		ctcString.append("// " + testName + "\n");
+		ctcString.append("//--------------------------------------------------------------------------------" + "\n");
+		ctcString.append("\n");
 
-
-		//--------------------------------------------------------------------------------
 		String preamble = ctc.getPreamble();
 		String preambleWithOutPackageAndImports = "";
 
@@ -48,124 +42,43 @@ public class CTCPrinter {
 		String preambleLine;
 		try {
 			while ((preambleLine = preambleReader.readLine()) != null) {
-				if (preambleLine.contains(packageChar) || preambleLine.contains(importChar)) {
-					ctcString = ctcString.append(preambleLine);
-					ctcString = ctcString.append("\n");
-				} else {
-					preambleWithOutPackageAndImports =
-							preambleWithOutPackageAndImports.concat(preambleLine) + "\n";
-				}
+				if (preambleLine.contains(packageChar) || preambleLine.contains(importChar)) 
+					ctcString.append(preambleLine + "\n");
+				else 
+					preambleWithOutPackageAndImports.concat(preambleLine + "\n");
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		//--------------------------------------------------------------------------------
 
-
-		//--------------------------------------------------------------------------------
 		// PRINT the TEST CLASS HEADER
-		//--------------------------------------------------------------------------------
-		ctcString = ctcString.append("\n");
-		ctcString = ctcString.append("public class ");
-		ctcString = ctcString.append(testName);			// SCHEMA NAME
-		ctcString = ctcString.append("\n");
-		ctcString = ctcString.append("{");
-		ctcString = ctcString.append("\n");
-		//--------------------------------------------------------------------------------
-
-
-		//--------------------------------------------------------------------------------
+		ctcString.append("\npublic class " + testName + "\n{\n");
+		
 		// CONCATENATES the PREAMBLE WITHOUT PACKAGE and IMPORTS
-		//--------------------------------------------------------------------------------
 		ctcString = ctcString.append(preambleWithOutPackageAndImports);
-		//--------------------------------------------------------------------------------
-
-
-		//--------------------------------------------------------------------------------
+		
 		// PRINT the RUNTEST CLASS HEADER
-		//--------------------------------------------------------------------------------
-		ctcString = ctcString.append("\n");
-		ctcString = ctcString.append("\t");
-		ctcString = ctcString.append("public static void main(String[] args)");
-		//ctcString = ctcString.append("public static void runTest_");
-		//ctcString = ctcString.append(testName);			// SCHEMA NAME
-		//ctcString = ctcString.append("_TCASE()");
-		ctcString = ctcString.append("\n");
-		ctcString = ctcString.append("\t");
-		ctcString = ctcString.append("{");
-		ctcString = ctcString.append("\n");
-		ctcString = ctcString.append("try{\n");
-		//--------------------------------------------------------------------------------
-
-
-		//--------------------------------------------------------------------------------
+		ctcString = ctcString.append("\npublic static void main(String[] args)\n{\ntry{\n");
+		
 		// CONCATENATES the REFINEMENT TEXT of ALL ASSIGNMENTS
-		//--------------------------------------------------------------------------------
-
-		if (ctc.getDeclaraciones()!=null)
-			ctcString = ctcString.append(ctc.getDeclaraciones());
-		if (ctc.getAsignaciones()!=null)
-			ctcString = ctcString.append(ctc.getAsignaciones());
-
-		//        List<TCaseAssignment> assignments = ctc.getAssigments();
-		//        Iterator<TCaseAssignment> iterAssignments = assignments.iterator();
-		//        TCaseAssignment assignment;
-		//        String assignmentRefText;
-		//
-		//        while (iterAssignments.hasNext()) {
-		//            assignment = iterAssignments.next();
-		//
-		//            assignmentRefText = assignment.getRefText();
-		//
-		//            // CONCATS the REFIMENT TEXT of ASSIGNMENT
-		//            ctcString = ctcString.append(
-		//                    "\t\t" // INDENTATION
-		//                    + assignmentRefText + "\n" + "\n");
-		//        }
-		//--------------------------------------------------------------------------------
-
-
-		//--------------------------------------------------------------------------------
+		if (ctc.getDeclaraciones()!=null) ctcString.append(ctc.getDeclaraciones());
+		if (ctc.getAsignaciones()!=null)  ctcString.append(ctc.getAsignaciones());
+		
 		// CONCATS the EPILOGUE
-		//--------------------------------------------------------------------------------
-		ctcString = ctcString.append("\t\t" + "//--------------------------------------------------------------------------------" + "\n");
-		ctcString = ctcString.append("\t\t" + "//                                   EPILOGUE" + "\n");
-		ctcString = ctcString.append("\t\t" + "//--------------------------------------------------------------------------------" + "\n");
-
 		String epilogue = ctc.getEpilogue();
-		BufferedReader epilogueReader = new BufferedReader(new StringReader(epilogue));
-		String epilogueLine;
-
-		try {
-			while ((epilogueLine = epilogueReader.readLine()) != null) {
-				ctcString = ctcString.append("\t\t" // INDENTATION
-						+ epilogueLine + "\n");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
+		if (!epilogue.isEmpty() || !epilogue.equals("")){
+			ctcString = ctcString.append("//--------------------------------------------------------------------------------\n");
+			ctcString = ctcString.append("//                                   EPILOGUE\n");
+			ctcString = ctcString.append("//--------------------------------------------------------------------------------\n");
+			ctcString.append(epilogue + "\n");
 		}
-		//--------------------------------------------------------------------------------
-
-		//--------------------------------------------------------------------------------
+		
 		// CLOSE the CATCH BLOCK.
-		//--------------------------------------------------------------------------------
-		ctcString = ctcString.append("}\n");
-		ctcString = ctcString.append("catch(Exception e){\n");
-		ctcString = ctcString.append("\te.printStackTrace(System.out);\n}");
-		//--------------------------------------------------------------------------------
+		ctcString = ctcString.append("}\ncatch(Exception e){\n\te.printStackTrace(System.out);\n\t}");
 		// CLOSE the RUNTEST CLASS.
-		//--------------------------------------------------------------------------------
-		ctcString = ctcString.append("\t");
-		ctcString = ctcString.append("}");
-		ctcString = ctcString.append("\n");
-		//--------------------------------------------------------------------------------
-
-
-		//--------------------------------------------------------------------------------
+		ctcString = ctcString.append("\n}\n");
 		// CLOSE the TEST CLASS.
-		//--------------------------------------------------------------------------------
 		ctcString = ctcString.append("}");
-		//--------------------------------------------------------------------------------
 
 		return ctcString.toString();
 	}
--- Fastest/src/client/presentation/commands/ShowHelpCommand.java
+++ Fastest/src/client/presentation/commands/ShowHelpCommand.java
@@ -10,72 +10,75 @@ import client.presentation.ClientTextUI;
  */
 public class ShowHelpCommand implements Command{ 
 
-    /**
-     * Runs this command.
-     * @param clientTextUI
-     * @param args
-     */    
-    @Override
+	/**
+	 * Runs this command.
+	 * @param clientTextUI
+	 * @param args
+	 */    
+	@Override
 	public void run(ClientTextUI clientTextUI, String args){
 
-	PrintWriter out = clientTextUI.getOutput();
-        String dots = "-----------------------------------";
-	out.println("\n" + dots + "Fastest Help" + dots);
-        out.println("Fastest commands:\n");
-        
-        out.println("loadspec <file_name>");
-        out.println("\tReads a Z specification into Fastest.\n");
-        
-        out.println("showspec [-u] [-o <file_name>]");	
-        out.println("\tShows the loaded specification.\n");
-        
-	out.println("showloadedops");
-        out.println("\tShows the operation names of the loaded specification.\n");
-        
-	out.println("selop <op_name>");
-	out.println("\tSelects the specified operation as one to be tested.\n");
-        
-	out.println("showselops");
-	out.println("\tShows all selected operations to be tested.\n");
-        
-	out.println("unselop <op_name>");	
-        out.println("\tRemoves the selection to be tested from the specified " +
-                "operation.\n");
-        
-	out.println("unselallops");
-        out.println("\tRemoves the selection to be tested from all the operations.\n");    
-        
-	out.println("selpred <sch_name>");
-	out.println("\tSelects the specified schema as a predicate.\n");
-        
-	out.println("showselpreds");
-	out.println("\tShows all schemas selected as predicates.\n");
-        
-	out.println("unselpred <sch_name>");
-        out.println("\tRemoves the selection, as a predicate, of the specified"
-                + " schema.\n");
-        
-	out.println("unselallpreds");
-	out.println("\tRemoves the selection, as predicates, of all the schemas.\n");
-        
-        out.println("showtactics");	
-        out.println("\tShows the available testing tactics in Fastest.\n");
-        
-        out.println("addtactic <op_name> <tactic_name> <tactic_parameters>");
-	out.println("\tAdds a tactic and its parameters to the specified operation.\n");
-        
-	out.println("genalltt");	
-	out.println("\tGenerates the test tree for every selected operation.\n");
-        
-        out.println("prunefrom <test_class_name>");
-        out.println("\tPrunes a test tree from the specified test class.\n");
-        
-        out.println("prunebelow <test_class_name>");
-        out.println("\tPrunes a test tree below the specified test class.\n");
-        
-        out.println("unprune <test_class_name>");	
-        out.println("\tUnprunes a test tree from the specified test class.\n");
-/*        
+		PrintWriter out = clientTextUI.getOutput();
+		String dots = "-----------------------------------";
+		out.println("\n" + dots + "Fastest Help" + dots);
+		out.println("Fastest commands:\n");
+
+		out.println("loadspec <file_name>");
+		out.println("\tReads a Z specification into Fastest.\n");
+		
+		out.println("loadrefrule <file_name>");
+		out.println("\tReads a file containing FTCRL programs.\n");
+
+		out.println("showspec [-u] [-o <file_name>]");	
+		out.println("\tShows the loaded specification.\n");
+
+		out.println("showloadedops");
+		out.println("\tShows the operation names of the loaded specification.\n");
+
+		out.println("selop <op_name>");
+		out.println("\tSelects the specified operation as one to be tested.\n");
+
+		out.println("showselops");
+		out.println("\tShows all selected operations to be tested.\n");
+
+		out.println("unselop <op_name>");	
+		out.println("\tRemoves the selection to be tested from the specified " +
+				"operation.\n");
+
+		out.println("unselallops");
+		out.println("\tRemoves the selection to be tested from all the operations.\n");    
+
+		out.println("selpred <sch_name>");
+		out.println("\tSelects the specified schema as a predicate.\n");
+
+		out.println("showselpreds");
+		out.println("\tShows all schemas selected as predicates.\n");
+
+		out.println("unselpred <sch_name>");
+		out.println("\tRemoves the selection, as a predicate, of the specified"
+				+ " schema.\n");
+
+		out.println("unselallpreds");
+		out.println("\tRemoves the selection, as predicates, of all the schemas.\n");
+
+		out.println("showtactics");	
+		out.println("\tShows the available testing tactics in Fastest.\n");
+
+		out.println("addtactic <op_name> <tactic_name> <tactic_parameters>");
+		out.println("\tAdds a tactic and its parameters to the specified operation.\n");
+
+		out.println("genalltt");	
+		out.println("\tGenerates the test tree for every selected operation.\n");
+
+		out.println("prunefrom <test_class_name>");
+		out.println("\tPrunes a test tree from the specified test class.\n");
+
+		out.println("prunebelow <test_class_name>");
+		out.println("\tPrunes a test tree below the specified test class.\n");
+
+		out.println("unprune <test_class_name>");	
+		out.println("\tUnprunes a test tree from the specified test class.\n");
+		/*        
         out.println("setfinitemodel <tclass_name> [-size <size>] " +
                 "[-btsize <size>]\n \t[-nzsize <size>] [-fm <list_of_finite_models>]");
         out.println("\tSets the finite model for the specified test class.\n");
@@ -101,35 +104,44 @@ public class ShowHelpCommand implements Command{
                 "axiomatic definition.\n");
         out.println("genalltca");        
 	out.println("\tGenerates the test cases for every selected operation.\n");
- * 
- */
-	out.println("showtt [-p <op_name>] [-o <file_name>] [-x]");
-	out.println("\tShows the previously generated test trees.\n");
-        
-        out.println("showsch <sch_name> [-u <unfold_order>] [-o <file_name>]");
-	out.println("\tShows the specified schema.\n");
-        
-        out.println("showsch -tcl [-p <op_name>] [-u <unfold_order>] [-o <file_name>]");
-	out.println("\tShows the previously generated test classes.\n");
-/*        
+		 * 
+		 */
+		out.println("showtt [-p <op_name>] [-o <file_name>] [-x]");
+		out.println("\tShows the previously generated test trees.\n");
+
+		out.println("showsch <sch_name> [-u <unfold_order>] [-o <file_name>]");
+		out.println("\tShows the specified schema.\n");
+
+		out.println("showsch -tcl [-p <op_name>] [-u <unfold_order>] [-o <file_name>]");
+		out.println("\tShows the previously generated test classes.\n");
+		
+		out.println("showctc <name>|<all> [-o <target_folder>]");
+		out.println("\tShows the specified (or all) refinement test case, <name> could be a tree node, an operation schema or a concrete test case." +
+				" A previously calling to showtt would be helpful .\n");
+		
+		out.println("refine <name> to test <path> implemented in <pl> with <frr>");
+		out.println("\tRefine the Z operation pecified by <name>. <path> is the UUT." +
+				" <pl> is the program language target. <frr> is the name oh a refinement rule.\n");
+		
+		/*        
         out.println("showsch -tca [-p <op_name>] [-u <unfold_order>] [-o <file_name>]");
 	out.println("\tShows the previously generated test cases.\n");
         out.println("eval <term>");        
 	out.println("\tEvaluates the specified term.\n");
- * 
- */
-	out.println("version");
-	out.println("\tDisplays the current version of Fastest.\n");
-        
-    	out.println("help");
-    	out.println("\tDisplays this help summary.\n");
-        
-       	out.println("reset");
-       	out.println("\tRemoves all specification and testing information.\n");
-        
-    	out.println("quit / exit");
-    	out.println("\tExits the Fastest program.");
-    	out.println();
+		 * 
+		 */
+		out.println("version");
+		out.println("\tDisplays the current version of Fastest.\n");
+
+		out.println("help");
+		out.println("\tDisplays this help summary.\n");
+
+		out.println("reset");
+		out.println("\tRemoves all specification and testing information.\n");
+
+		out.println("quit / exit");
+		out.println("\tExits the Fastest program.");
+		out.println();
 	}
 
 } 
