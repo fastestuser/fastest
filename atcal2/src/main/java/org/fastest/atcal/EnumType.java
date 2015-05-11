ae3a009d484377682e8d24cce4b7853a8d1d31e3
Cristian Rosa <cristian@automative.io>
2015-05-11 10:26:20 -0300

    Improve int to enum conversion.

--- atcal2/src/main/java/org/fastest/atcal/EnumType.java
+++ atcal2/src/main/java/org/fastest/atcal/EnumType.java
@@ -1,17 +1,12 @@
 package org.fastest.atcal;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Maps;
-import org.fastest.atcal.apl.APLExpr;
 import org.fastest.atcal.apl.ConsExpr;
 import org.fastest.atcal.z.ast.ZExpr;
 import org.fastest.atcal.z.ast.ZExprNum;
 
 import java.util.Collection;
 import java.util.LinkedHashMap;
-import java.util.Map;
 
 /**
  * Created by Cristian on 05/05/15.
@@ -24,7 +19,7 @@ public class EnumType implements ATCALType {
     public EnumType(String name, Collection<String> elements) {
         this.name = name;
         this.elements = Maps.newLinkedHashMap();
-        for(String e: elements)
+        for (String e : elements)
             this.elements.put(e, new ConsExpr(e));
     }
 
@@ -32,20 +27,31 @@ public class EnumType implements ATCALType {
         return name;
     }
 
-    public ConsExpr getElem(String e){
+    public ConsExpr getElem(String e) {
         return elements.get(e);
     }
 
     // Conversion to enum constants
     public ConsExpr fromZExpr(ZExpr zExpr) throws Exception {
-        if (zExpr instanceof ZExprNum){
-            return (ConsExpr)elements.values().toArray()[(int)((ZExprNum)zExpr).getNum()];
+
+        // Convert from Z numeric expression
+        if (zExpr instanceof ZExprNum) {
+            ZExprNum zExprNum = (ZExprNum) zExpr;
+
+            // Check if the numeric expression value is between the bounds of the array.
+            int value = (int) zExprNum.getNum();     // TODO: what happens if beyond the int range?
+            if (value < 0 && value > elements.values().size())
+                throw new Exception();
+
+            return (ConsExpr) elements.values().toArray()[value];
         }
+
+        // Unsupported conversion
         throw new Exception();
     }
 
     @Override
     public String toString() {
-        return "Enum " + name +  "(" + elements.values() + ")";
+        return "Enum " + name + "(" + elements.values() + ")";
     }
 }
