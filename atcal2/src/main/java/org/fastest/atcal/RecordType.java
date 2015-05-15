5ba88cd2db9a9ffcdf2ec75feb6af5a8c121aa5d
Cristian Rosa <cristian@automative.io>
2015-05-15 14:39:57 -0300

    Improve implementation of record types.

--- atcal2/src/main/java/org/fastest/atcal/RecordType.java
+++ atcal2/src/main/java/org/fastest/atcal/RecordType.java
@@ -3,10 +3,24 @@ package org.fastest.atcal;
 import org.fastest.atcal.apl.APLExpr;
 import org.fastest.atcal.z.ast.ZExpr;
 
+import java.util.Map;
+import java.util.Optional;
+
 /**
- * Created by cristian on 06/05/15.
+ * Created by Cristian on 06/05/15.
  */
 public class RecordType implements ATCALType {
+
+
+    private final Map<String, ATCALType> fields;
+
+    public RecordType(Map<String, ATCALType> fields) {
+        this.fields = fields;
+    }
+
+    public Optional<ATCALType> getFieldType(String field){
+        return Optional.ofNullable(fields.get(field));
+    }
 
     public APLExpr fromZExpr(ZExpr expr) throws Exception {
         throw new Exception();
