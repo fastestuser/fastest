d501e3e7ca909b492393b93e63dc8c5e42621295
Cristian Rosa <cristian@automative.io>
2015-06-05 14:40:52 -0300

    Add javadoc documentation.

--- atcal2/src/main/java/org/fastest/atcal/ATCALType.java
+++ atcal2/src/main/java/org/fastest/atcal/ATCALType.java
@@ -7,6 +7,11 @@ import org.fastest.atcal.z.ast.ZExpr;
  * Created by Cristian on 4/21/15.
  */
 public abstract class ATCALType {
+    /**
+     * Convert a Z expression into an APL
+     * @param expr  The Z expression
+     * @return      An APL expression or a runtime exception if conversion is not supported.
+     */
     public APLExpr fromZExpr(ZExpr expr) {
         throw new RuntimeException("Unsupported operation.");
     }
