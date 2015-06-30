3c8e44b49f6aa931438a2cc678065ef402065145
Cristian Rosa <cristian@automative.io>
2015-06-30 15:51:34 -0300

    Add perl code generator.

---
+++ atcal2/src/test/java/PerlGenTest.java
@@ -0,0 +1,29 @@
+import com.google.common.collect.Lists;
+import org.fastest.atcal.ArrayType;
+import org.fastest.atcal.IntType;
+import org.fastest.atcal.apl.*;
+import org.fastest.atcal.generators.PerlGen;
+import org.junit.Test;
+
+/**
+ * Created by Cristian on 30/06/15.
+ */
+public class PerlGenTest {
+
+    @Test
+    public void test1() {
+        APLArray arr1 = new APLArray("myArray", new ArrayType(IntType.getInstance(), 10));
+        APLVar var1 = new APLVar("myVar", IntType.getInstance());
+        APLExpr longExpr = new LongExpr(6);
+        APLExpr constExpr = new ConsExpr("HELLO");
+        APLExpr stringExpr = new StringExpr("World");
+        CallExpr callExpr = new CallExpr("function", Lists.newArrayList("a", "b", "c"));
+
+        System.out.println(PerlGen.generate(new AssignStmt(var1, longExpr)));
+        System.out.println(PerlGen.generate(new AssignStmt(var1, constExpr)));
+        System.out.println(PerlGen.generate(new AssignStmt(arr1.getIndex(0), stringExpr)));
+        System.out.println(PerlGen.generate(new AssignStmt(var1, callExpr)));
+        System.out.println(PerlGen.generate(callExpr));
+    }
+
+}
