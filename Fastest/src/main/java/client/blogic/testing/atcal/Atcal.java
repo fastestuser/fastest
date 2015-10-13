4d38b36d141dff45f9e76746492b71d4fdcce5aa
Cristian Rosa <cristian@automative.io>
2015-10-13 09:10:59 -0300

    Move function to a public utility class.

--- Fastest/src/main/java/client/blogic/testing/atcal/Atcal.java
+++ Fastest/src/main/java/client/blogic/testing/atcal/Atcal.java
@@ -2,25 +2,37 @@ package client.blogic.testing.atcal;
 
 import client.blogic.testing.atcal.parser.AtcalLexer;
 import client.blogic.testing.atcal.parser.AtcalParser;
+import client.blogic.testing.atcal.z.ast.CZTTranslator;
+import client.blogic.testing.atcal.z.ast.ZExpr;
+import client.blogic.testing.atcal.z.ast.ZExprSchema;
+import client.blogic.testing.atcal.z.ast.ZVar;
+import common.z.AbstractTCase;
+import net.sourceforge.czt.z.ast.Expr;
+import net.sourceforge.czt.z.ast.RefExpr;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Created by cristian on 3/31/15.
  */
 public class Atcal {
 
-    public static void main(String[] args) throws Exception {
-        // Create a CharStream that reads from standard input
-        ANTLRInputStream input = new ANTLRInputStream(System.in);
-        // Create a lexer that feeds off of input CharStream
-        AtcalLexer lexer = new AtcalLexer(input);
-        // Create a buffer of tokens pulled from the lexer
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        // Create a parser that feeds off the tokens buffer
-        AtcalParser parser = new AtcalParser(tokens);
-        ParseTree tree = parser.refinementRule(); // begin parsing at init rule
-        System.out.println(tree.toStringTree(parser)); // print LISP-style tree
+    /**
+     * Helper function to translate the abstract test case from CZT to ATCAL representation.
+     * @param atc   a CZT abstract test case
+     * @return      an ATCAL abstract test case
+     */
+    public static ZExprSchema ATCToZExpr(AbstractTCase atc) {
+        final CZTTranslator cztTranslator = new CZTTranslator();
+        ArrayList<ZVar> translatedVars = new ArrayList<ZVar>();
+        for (Map.Entry<RefExpr, Expr> varExpr : atc.getVarExpMap().entrySet()) {
+            ZExpr expr = varExpr.getValue().accept(cztTranslator);
+            translatedVars.add(new ZVar(varExpr.getKey().getName().toString(), expr));
+        }
+        return new ZExprSchema(translatedVars.toArray(new ZVar[translatedVars.size()]));
     }
 }
--- Fastest/src/main/java/client/blogic/testing/atcal/TCaseRefineClientRunner.java
+++ Fastest/src/main/java/client/blogic/testing/atcal/TCaseRefineClientRunner.java
@@ -45,21 +45,6 @@ public class TCaseRefineClientRunner implements Runnable {
         this.refinementRule = refinementRule;
     }
 
-    /**
-     * Helper function to translate the abstract test case from CZT to ATCAL representation.
-     * @param atc   a CZT abstract test case
-     * @return      an ATCAL abstract test case
-     */
-    private static ZExprSchema ATCToZExpr(AbstractTCase atc) {
-        final CZTTranslator cztTranslator = new CZTTranslator();
-        ArrayList<ZVar> translatedVars = new ArrayList<ZVar>();
-        for (Map.Entry<RefExpr, Expr> varExpr : atc.getVarExpMap().entrySet()) {
-            ZExpr expr = varExpr.getValue().accept(cztTranslator);
-            translatedVars.add(new ZVar(varExpr.getKey().getName().toString(), expr));
-        }
-        return new ZExprSchema(translatedVars.toArray(new ZVar[translatedVars.size()]));
-    }
-
     @Override
     public void run() {
 
@@ -73,7 +58,7 @@ public class TCaseRefineClientRunner implements Runnable {
             throw new RuntimeException("The " + targetLanguage + " language is not supported by the testing backend.");
 
         // Translate the abstract test case to ATCAL AST.
-        ZExprSchema atc = ATCToZExpr(abstractTCase);
+        ZExprSchema atc = Atcal.ATCToZExpr(abstractTCase);
 
         String concreteTCaseName = SpecUtils.getAxParaName(abstractTCase).replaceAll("_TCASE", "_CTCASE");
 
