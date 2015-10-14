d459e47b647b38a0957a34dad258e52c16cc4240
Cristian Rosa <cristian@automative.io>
2015-10-14 13:29:16 -0300

    Add assertion to unit test.

--- Fastest/src/test/java/CZTTranslatorTest.java
+++ Fastest/src/test/java/CZTTranslatorTest.java
@@ -13,17 +13,17 @@ public class CZTTranslatorTest {
     public void test() {
         ZExprSchema atc = Atcal.parseATCFile("CZTTranslatorTest/atc1.tex");
 
-        ZVar a = new ZVar("a", new ZExprNum(1));
-        ZVar b = new ZVar("b", ZExprSet.of(new ZExprNum(1), new ZExprNum(2), new ZExprNum(3), new ZExprNum(4)));
-        ZVar d = new ZVar("d", ZExprSet.of(new ZExprConst("h", ZExprConst.ConstantType.BASIC),
-                new ZExprConst("i", ZExprConst.ConstantType.BASIC), new ZExprConst("j", ZExprConst.ConstantType.BASIC)));
-        ZVar e = new ZVar("e", ZExprProd.of(new ZExprNum(1), new ZExprNum(2)));
-        ZVar f = new ZVar("f", ZExprSet.of(ZExprProd.of(new ZExprConst("x", ZExprConst.ConstantType.BASIC), new ZExprNum(1)),
-                ZExprProd.of(new ZExprConst("y", ZExprConst.ConstantType.BASIC), new ZExprNum(2))));
-        ZVar k = new ZVar("k", new ZExprList(Arrays.asList(new ZExprNum(1), new ZExprNum(2), new ZExprNum(3), new ZExprNum(4))));
+        ZExprSchema atcTest = new ZExprSchema.Builder().
+                addNumVar("a", 1).
+                addVar("b", ZExprSet.of(new ZExprNum(1), new ZExprNum(2), new ZExprNum(3), new ZExprNum(4))).
+                addVar("d", ZExprSet.of(ZExprConst.basic("h"), ZExprConst.basic("i"), ZExprConst.basic("j"))).
+                addVar("e", ZExprProd.of(new ZExprNum(1), new ZExprNum(2))).
+                addVar("f", ZExprSet.of(ZExprProd.of(ZExprConst.basic("x"), new ZExprNum(1)), ZExprProd.of(ZExprConst.basic("y"), new ZExprNum(2)))).
+                addVar("k", new ZExprList(Arrays.asList(new ZExprNum(1), new ZExprNum(2), new ZExprNum(3), new ZExprNum(4)))).build();
+
 //        System.out.println(atc);
 //        System.out.println(ZExprSchema.add(ZExprSchema.of(a,b,d,e,f), k));
-        assert(ZExprSchema.add(ZExprSchema.of(a,b,d,e,f), k).equals(atc));
+        assert (atcTest.equals(atc));
     }
 
 }
