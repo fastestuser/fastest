1926741eacd25faa202d626f601de7dc47100858
Cristian Rosa <cristian@automative.io>
2015-11-03 14:47:58 -0300

    Remove unused code.

--- Fastest/src/test/java/PerlGenTest.java
+++ Fastest/src/test/java/PerlGenTest.java
@@ -4,19 +4,8 @@ import client.blogic.testing.atcal.ConcreteTCase;
 import client.blogic.testing.atcal.generators.PerlGen;
 import client.blogic.testing.atcal.parser.AtcalLexer;
 import client.blogic.testing.atcal.parser.AtcalParser;
-import client.blogic.testing.atcal.z.ast.CZTTranslator;
 import client.blogic.testing.atcal.z.ast.ZExprSchema;
-import client.blogic.testing.atcal.z.ast.ZVar;
-import com.google.common.base.Charsets;
-import com.google.common.collect.Lists;
 import com.google.common.io.Resources;
-import common.z.czt.UniqueZLive;
-import net.sourceforge.czt.animation.eval.ZLive;
-import net.sourceforge.czt.parser.circus.ParseUtils;
-import net.sourceforge.czt.session.CommandException;
-import net.sourceforge.czt.session.StringSource;
-import net.sourceforge.czt.z.ast.Expr;
-import net.sourceforge.czt.z.ast.RefExpr;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
@@ -24,8 +13,6 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
